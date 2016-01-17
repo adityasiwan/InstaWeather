69a76ea203734f1e271c89da7677a4933162da9b
Aditya Harsh <harsh.aditya14@stu.upes.ac.in>
2016-01-17 15:42:26 +0530

    Create ArrayAdapter to eventually use to populate the ListView

--- app/src/main/java/com/example/android/instaweather/MainActivity.java
+++ app/src/main/java/com/example/android/instaweather/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,18 +13,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
